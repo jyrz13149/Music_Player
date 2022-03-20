c1412364a9d10bf7b4155386e2826fca5b2baccc
jyrz13149 <90429235+jyrz13149@users.noreply.github.com>
2022-03-20 04:27:34 -0400

    t

--- Music_Player_DB.sql
+++ Music_Player_DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 16, 2022 at 11:06 PM
+-- Generation Time: Mar 20, 2022 at 09:25 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.2
 
@@ -37,13 +37,6 @@ CREATE TABLE `music_information` (
   `link` varchar(1024) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `music_information`
---
-
-INSERT INTO `music_information` (`id`, `song_name`, `artist_name`, `album_name`, `genre`, `release_year`, `link`) VALUES
-(1, 'Dynamite', 'BTS', 'BE', 'K-POP', '2020', 'bts.com/dynamite');
-
 -- --------------------------------------------------------
 
 --
@@ -60,15 +53,9 @@ CREATE TABLE `user_information` (
   `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `verification_code` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `verification_status` int(11) NOT NULL DEFAULT 0,
-  `pass_reset_token` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL
+  `pass_reset_token` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `playlist_status` int(2) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Mainly for Creating Account & Login';
-
---
--- Dumping data for table `user_information`
---
-
-INSERT INTO `user_information` (`id`, `email`, `password`, `mm_name`, `nick_name`, `born_city`, `date`, `verification_code`, `verification_status`, `pass_reset_token`) VALUES
-(30, 'alfy001118@gmail.com', '$2y$10$3L4g2sAJdaHCO2hRyRIJhOP0t.EwU3EsZ2e8wChNXk6qP/1dsIcUq', 'something', 'something', 'somewhere', '2022-03-16 20:50:16', '7d402036b16476581b0161c24928ff11', 1, NULL);
 
 --
 -- Indexes for dumped tables
