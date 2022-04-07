e944a1bf667a5593fbfb263bc702370ddaff041e
Brandon Spangler <94080573+brandonspangler2@users.noreply.github.com>
2022-04-07 15:10:17 -0400

    All songs

--- music_information.sql
+++ music_information.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 31, 2022 at 06:58 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.1.2
+-- Host: localhost:3306
+-- Generation Time: Apr 07, 2022 at 12:56 PM
+-- Server version: 5.7.23-23
+-- PHP Version: 7.3.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Music_Player_DB`
+-- Database: `brandsg3_music_player_db`
 --
 
 -- --------------------------------------------------------
@@ -30,10 +31,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `music_information` (
   `id` int(11) NOT NULL,
   `song_name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `artist_name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `album_name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `genre` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  `release_year` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `artist_name` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `album_name` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `genre` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `release_year` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
   `link` varchar(1024) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -42,98 +43,56 @@ CREATE TABLE `music_information` (
 --
 
 INSERT INTO `music_information` (`id`, `song_name`, `artist_name`, `album_name`, `genre`, `release_year`, `link`) VALUES
-(1, 'Heating Up', 'Polo G', '', 'Hip-Hop', '2022', '../music_link/Polo_G_Heating_up.mp3'),
-(2, 'Baby', 'Justin Bieber', 'My World 2.0', 'Pop', '2010', '../music_link/Justin_Bieber_Baby_ft_Ludacris.mp3'),
-(3, 'Somebody To Love', 'Justin Bieber', 'My World 2.0', 'Pop', '2010', '../music_link/Somebody_To_Love.mp3'),
-(4, 'Peaches (feat. Daniel Caesar & Giveon)', 'Justin Bieber, Daniel Caesar, Giveon', 'Justice', 'R&B', '2021', '../music_link/Justin_Bieber_Peaches_ft_Daniel_Caesar_Giveon.mp3'),
-(5, 'STAY', 'The Kid LAROI', '', 'Pop', '2021', '../music_link/The_Kid_LAROI_Justin_Bieber_STAY.mp3'),
-(6, 'God\'s Plan', 'Drake', 'Scorpion', 'Hip-Hop', '2018', '../music_link/Gods_Plan.mp3'),
-(7, 'No Role Modelz', 'J. Cole', '2014 Forest Hills Drive', 'Hip-Hop', '2014', '../music_link/No_Role_Modelz.mp3'),
-(8, 'Statement', 'G Herbo', '', 'Hip-Hop', '2020', '../music_link/G_Herbo_Statement.mp3'),
-(9, 'Empty', 'Juice WRLD', 'Death Race For Love', 'Hip-Hop', '2019', '../music_link/Juice_WRLD_Empty.mp3'),
-(10, 'For Tonight', 'Giveon', '', 'R&B', '2021', '../music_link/Giveon_For_Tonight.mp3'),
-(11, 'Home', 'SEVENTEEN', '', 'K-Pop', '2019', '../music_link/SEVENTEEN_Home.mp3'),
-(12, 'Controlla', 'Drake', 'Views', 'R&B', '2016', '../music_link/Drake_Controlla.mp3'),
-(13, 'Wants and Needs (feat. Lil Baby)', 'Drake, Lil Baby', '', 'Hip-Hop', '2021', '../music_link/Wants_and_Needs.mp3'),
-(14, 'Fine China', 'Future, Juice WRLD', 'Future & juice WRLD Present... WRLD ON DRUGS', 'Hip-Hop', '2018', '../music_link/Future_Juice_WRLD_Fine_China.mp3'),
-(15, 'Cigarettes', 'Juice WRLD', 'Fighting Demons', 'Rock', '2021', '../music_link/Juice_WRLD_Cigarettes.mp3'),
-(16, 'Never Recover (Lil Baby & Gunna, Drake)', 'Lil Baby, Gunna, Drake', 'Drip Harder', 'Hip-Hop', '2018', '../music_link/LilBaby_Gunna_Drake_Never_Recover.mp3'),
-(17, 'Mask Off', 'Future', 'Future', 'Hip-Hop', '2017', '../music_link/Future_Mask_Off.mp3'),
-(18, 'bloody valentine', 'Machine Gun Kelly', 'Tickets To My DownFall', 'Rock', '2020', '../music_link/Machine_Gun_Kelly_Bloody_Valentine.mp3'),
-(19, 'parents', 'YUNGBLUD', 'weird!', 'Rock', '2020', '../music_link/yungblud_parents.mp3'),
-(20, 'Look What God Gave Her', 'Thomas Rhett', 'Center Point Road', 'Country', '2019', '../music_link/Thomas_Rhett_Look_What_God_Gave_Her.mp3'),
-(21, 'Old Town Road', 'Lil Nas X', '', 'Country', '2017', '../music_link/Lil_Nas_X_Old_Town_Road_ft_Billy_Ray_Cyrus.mp3'),
-(22, 'Loud', 'R5', 'Louder', 'Rock', '2013', '../music_link/R5_Loud.mp3'),
-(23, 'Forget About You', 'R5', 'Louder', 'Rock', '2013', '../music_link/R5_Forget_About_You.mp3'),
-(24, 'Company', 'Justin Bieber', 'Purpose', 'Pop', '2015', '../music_link/Justin_Bieber_Company.mp3'),
-(26, 'Lovesick Girls', 'Blackpink', 'The Album', 'K-POP', '2020', '../music_link/Blackpink_Lovesick_Girls.mp3'),
-(27, 'Ddu-Du Ddu-Du', 'Blackpink', 'Square Up - EP', 'K-POP', '2018', '../music_link/Blackpink_Ddu_Du_Ddu_Du.mp3'),
-(28, 'As If It\'s Your Last', 'Blackpink', 'As If It\'s Your Last - Single', 'K-POP', '2017', '../music_link/Blackpink_As_If_Its_Your_Last.mp3'),
-(29, 'Beautiful Pain', 'BTOB', 'Hour Moment - EP', 'K-POP', '2018', '../music_link/BTOB_Beautiful_Pain.mp3'),
-(30, 'Insane', 'BTOB', 'Born TO Beat', 'K-POP', '2012', '../music_link/BTOB_Insane.mp3'),
-(31, 'Missing You', 'BTOB', 'Brother Act.', 'K-POP', '2017', '../music_link/BTOB_Missing_You.mp3'),
-(32, 'Movie', 'BTOB', 'Feel\'eM - EP', 'K-POP', '2017', '../music_link/BTOB_Movie.mp3'),
-(33, 'Only One For Me', 'BTOB', 'This Is Us', 'K-POP', '2018', '../music_link/BTOB_Only_One_For_Me.mp3'),
-(34, 'Pray', 'BTOB', 'New Men', 'K-POP', '2016', '../music_link/BTOB_Pray.mp3'),
-(35, 'Boy With Luv (feat. Halsey)', 'BTS', 'Map Of The Soul: 7', 'K-POP', '2020', '../music_link/BTS_Boy_With_Luv.mp3'),
-(36, 'Dynamite', 'BTS', 'BE', 'K-POP', '2020', '../music_link/BTS_Dynamite.mp3'),
-(37, 'Euphoria', 'BTS', 'Love Yourself \'Answer\'', 'K-POP', '2018', '../music_link/BTS_Euphoria.mp3'),
-(38, 'Fake Love', 'BTS', 'Love Yourself \'Answer\'', 'K-POP', '2018', '../music_link/BTS_Fake_Love.mp3'),
-(39, 'Permission To Dance', 'BTS', 'Butter/Permission to Dance - EP', 'K-POP', '2021', '../music_link/BTS_Permission_To_Dance.mp3'),
-(40, 'Butter', 'BTS', 'Butter/Permission to Dance - EP', 'K-POP', '2021', 'music_link/BTS_Butter.mp3'),
-(41, 'Run', 'BTS', 'The Most Beautiful Moment In Life, Pt.2', 'K-POP', '2016', '../music_link/BTS_Run.mp3'),
-(42, 'Save Me', 'BTS', 'The Most Beautiful Moment In Life: Young Forever', 'K-POP', '2016', '../music_link/BTS_Save_Me.mp3'),
-(43, 'Crooked', 'G-Dragon', 'COUP D\'ETAT, Pt.2', 'K-POP', '2013', '../music_link/Gdragon_Crooked.mp3'),
-(44, 'Untitled, 2014', 'G-Dragon', 'Kwon Ji Yong - EP', 'K-POP', '2017', '../music_link/Gdragon_Untitled_2014.mp3'),
-(45, 'Mago', 'GFriend', 'Walpurgis Night', 'K-POP', '2020', '../music_link/Gfriend Mago.mp3'),
-(46, 'Time for the Moon Night', 'GFriend', 'GFriend the 6th Mini Album \'Time for the Moon Night\'', 'K-POP', '2018', '../music_link/Gfriend_Time_For_The_Moon_Night.mp3'),
-(47, 'Blueming', 'IU', 'Love Poem - EP', 'K-POP', '2019', '../music_link/IU_Blueming.mp3'),
-(48, 'Celebrity', 'IU', 'Celebrity - Single', 'K-POP', '2021', '../music_link/IU_Celebrity.mp3'),
-(49, 'eight (feat. SUGA)', 'IU', 'eight (feat. SUGA) - Single', 'K-POP', '2020', '../music_link/IU_Eight.mp3'),
-(50, 'Lilac', 'IU', 'IU 5th Album \'LILAC\'', 'K-POP', '2021', '../music_link/IU_Lilac.mp3'),
-(51, 'AYA', 'Mamamoo', 'Travel - EP', 'K-POP', '2020', '../music_link/Mamamoo_Aya.mp3'),
-(52, 'Dingga', 'Mamamoo', 'Dingga - Single', 'K-POP', '2020', '../music_link/Mamamoo_Dingga.mp3'),
-(53, 'Hip', 'Mamamoo', 'reality in BLACK', 'K-POP', '2019', '../music_link/Mamamoo_Hip.mp3'),
-(54, 'Face', 'NU\'EST', 'NU\'EST The First Album \"Re:BIRTH\"', 'K-POP', '2012', '../music_link/Nuest_Face.mp3'),
-(55, 'Nonstop', 'Oh My Girl', 'NONSTOP - EP', 'K-POP', '2020', '../music_link/Oh_My_Girl_Nonstop.mp3'),
-(56, 'Secret Garden', 'Oh My Girl', 'Secret Garden - EP', 'K-POP', '2018', '../music_link/Oh_My_Girl_Secret_Garden.mp3'),
-(57, '5th Season (SSFWL)', 'Oh My Girl', 'THE FIFTH SEASON', 'K-POP', '2019', '../music_link/Oh_My_Girl_The_5th_Season.mp3'),
-(58, 'Bad Boy', 'Red Velvet', 'The Perfect Red Velvet - The 2nd Album Repackage - EP', 'K-POP', '2018', '../music_link/Red_Velvet_Bad_Boy.mp3'),
-(59, 'Power Up', 'Red Velvet', 'Summer Magic - Summer Mini Album', 'K-POP', '2018', '../music_link/Red_Velvet_Power_Up.mp3'),
-(60, 'Psycho', 'Red Velvet', '\'The ReVe Festival\' Finale - EP', 'K-POP', '2019', '../music_link/Red_Velvet_Psycho.mp3'),
-(61, 'Red Flavor', 'Red Velvet', 'The Red Summer - Summer Mini Album - EP', 'K-POP', '2017', '../music_link/Red_Velvet_Red_Flavor.mp3'),
-(62, 'Umpah Umpah', 'Red Velvet', '\'The ReVe Festival\' Day 2 - EP', 'K-POP', '2019', '../music_link/Red_Velvet_Umpah_Umpah.mp3'),
-(63, 'Darling', 'Taeyang', 'White Night', 'K-POP', '2017', '../music_link/Taeyang_Darling.mp3'),
-(64, 'Wake Me Up', 'Taeyang', 'White Night', 'K-POP', '2017', '../music_link/Taeyang_Wake_Me_Up.mp3'),
-(65, 'MONTERO', 'Lil Nas X', 'MONTERO', 'Hip-Hop', '2021', '../music_link/MONTERO.mp3'),
-(66, 'SCOOP', 'Lil Nas X', 'MONTERO', 'Hip-Hop', '2021', '../music_link/SCOOP.mp3'),
-(67, 'DONT WANT IT', 'Lil Nas X', 'MONTERO', 'Hip-Hop', '2021', '../music_link/DONT_WANT_IT.mp3'),
-(68, 'SUN GOES DOWN', 'Lil Nas X', 'MONTERO', 'Hip-Hop', '2021', '../music_link/SUN_GOES_DOWN.mp3'),
-(69, 'THATS WHAT I WANT', 'Lil Nas X', 'MONTERO', 'Hip-Hop', '2021', '../music_link/THATS_WHAT_I_WANT.mp3'),
-(70, 'No Worries', 'Lil Wayne', 'I Am Not A Human Being II', 'Hip-Hop', '2013', '../music_link/No_Worries.mp3'),
-(71, 'Days And Days', 'Lil Wayne', 'I Am Not A Human Being II', 'Hip-Hop', '2013', '../music_link/Days_And_Days.mp3'),
-(72, 'Gunwalk', 'Lil Wayne', 'I Am Not A Human Being II', 'Hip-Hop', '2013', '../music_link/Gunwalk.mp3'),
-(73, 'Rich As Trucks', 'Lil Wayne', 'I Am Not A Human Being II', 'Hip-Hop', '2013', '../music_link/Rich_As_Trucks.mp3'),
-(74, 'Love Me', 'Lil Wayne', 'I Am Not A Human Being II', 'Hip-Hop', '2013', '../music_link/Love_Me.mp3'),
-(75, 'Jail', 'Kanye West', 'Donda', 'Hip-Hop', '2021', '../music_link/Jail.mp3'),
-(76, 'Off The Grid', 'Kanye West', 'Donda', 'Hip-Hop', '2021', '../music_link/Off_The_Grid.mp3'),
-(77, 'Praise God', 'Kanye West', 'Donda', 'Hip-Hop', '2021', '../music_link/Praise_God.mp3'),
-(78, 'Heaven and Hell', 'Kanye West', 'Donda', 'Hip-Hop', '2021', '../music_link/Heaven_and_Hell.mp3'),
-(79, 'Hurricane', 'Kanye West', 'Donda', 'Hip-Hop', '2021', '../music_link/Hurricane.mp3'),
-(80, 'Someone Like You', 'Adele', '21', 'Pop', '2011', '../music_link/Someone_like_you.mp3'),
-(81, 'Ain\'t No Mountain High Enough', 'Marvin Gaye', 'Forever Yours', 'Soul', '1967', '../music_link/mountain_high_enough.mp3'),
-(82, 'All Of The Lights', 'Kanye West', 'My Beautiful Dark Twisted Fantasy', 'Hip-Hop', '2010', '../music_link/All_Of_The_Lights.mp3'),
-(83, 'Remember The Name (feat. Eminem & 50 Cent)', 'Ed Sheeran', 'No.6 Collaborations Project', 'Pop', '2019', '../music_link/Remember_The Name.mp3'),
-(84, 'Hard Skool', 'Guns N\' Roses', 'Hard Skool - Single', 'Hard Rock', '2021', '../music_link/Hard_Skool.mp3'),
-(85, 'Hooked On A Feeling', 'Blue Swede', 'Hooked on a Feeling', 'Rock', '1974', '../music_link/Hooked_on_a_Feeling.mp3'),
-(86, 'My Life', 'Imagine Dragons', 'Mercury - Act I', 'Alternative', '2021', '../music_link/my_life.mp3'),
-(87, 'Jesus Walks', 'Kanye West', 'The College Dropout', 'Hip-Hop', '2004', '../music_link/Jesus_Walks.mp3'),
-(88, 'Black Summer', 'Red Hot Chili Peppers', 'Unlimited Love', 'Alternative', '2022', '../music_link/Black_Summer.mp3'),
-(89, 'Can\'t Stop', 'Red Hot Chili Peppers', 'By the Way', 'Alternative', '2002', '../music_link/Cant_Stop.mp3'),
-(90, 'Set Fire to the Rain', 'Adele', '21', 'Pop', '2011', '../music_link/Set_Fire_to_the_Rain.mp3'),
-(91, 'Stronger', 'Kanye West', 'Graduation', 'Hip-Hop', '2007', '../music_link/Stronger.mp3'),
-(92, 'Face Off (ft. The Rock)', 'Tech N9ne', 'ASIN9NE', 'Rap', '2021', '../music_link/Face_Off.mp3'),
-(93, 'Is There Someone Else', 'The Weeknd', 'Dawn FM', 'R&B', '2022', '../music_link/Is_There_Someone_Else.mp3');
+(1, 'A Lazy Farmer Boy', 'Buster Carter And Preston Young', NULL, 'New York', '1931', '../music_link/A_Lazy_Farmer_Boy.mp3'),
+(2, 'April Kisses', 'Eddie Lang', NULL, 'Waltz', NULL, '../music_link/April_Kisses.mp3'),
+(3, 'Charles Giteau', 'Kelly Harrell', NULL, 'Parallel Anthology', '1927', '../music_link/Charles_Giteau.mp3'),
+(4, 'Eddie\'s Twister', 'Eddie Lang', NULL, 'Instrumental', NULL, '../music_link/Eddies_Twister.mp3'),
+(5, 'In The Dark-Flashes', 'Jess Stacy', NULL, 'Instrumental', '1935', '../music_link/In_The_Dark_Flashes.mp3'),
+(6, 'Introduction and Tarantelle', 'Mischa Violin (Violin) Josef Adler', NULL, 'Classical', '1921', '../music_link/Intro_And_Tarantelle.mp3'),
+(7, 'My Name Is John Johanna', 'Kelly Harrell And The Virginia String Band', NULL, 'Parallel Anthology', '1927', '../music_link/My_Name_Is_John_Johanna.mp3'),
+(8, 'Pinetop\'s Boogie Woogie', 'Pine Top Smith', NULL, 'Vocal', '1928', '../music_link/Pinetops_Boogie_Woogie.mp3'),
+(9, 'Ragtime Annie', 'Charlie Poole and The North Carolina Ramblers Group', NULL, 'Instrumental', '1926', '../music_link/Ragtime_Annie.mp3'),
+(10, 'Waiting For A Train', 'Jimmy Rodgers', NULL, 'Country', '1928', '../music_link/Waiting_For_A_Train.mp3'),
+(11, 'The Edge', 'NIVIRO', NULL, 'Pop', '2022', '../music_link/The_Edge.mp3'),
+(12, 'Royalty (ft. Neoni)', 'Egzod & Maestro', NULL, 'Pop', '2021', '../music_link/Royalty.mp3'),
+(13, 'Arrow', 'Jim Yosef', NULL, 'Electronic', '2015', '../music_link/Arrow.mp3'),
+(14, 'Cetus', 'Lensko', NULL, 'Electronic', '2014', '../music_link/Cetus.mp3'),
+(15, 'Mesmerize', 'Tobu', NULL, 'Electronic', '2014', '../music_link/Mesmerize.mp3'),
+(16, 'Life', 'Tobu', NULL, 'Electronic', '2014', '../music_link/Life.mp3'),
+(17, 'Imagine', 'Jim Yosef', NULL, 'Electronic', '2018', '../music_link/Imagine.mp3'),
+(18, 'Cake', 'RetroVision', NULL, 'Pop', '2018', '../music_link/Cake.mp3'),
+(19, 'I Just Wanna', 'NIVIRO', NULL, 'Hip-Hop', '2018', '../music_link/I_Just_Wanna.mp3'),
+(20, 'Hurts Like This', 'EMDI', NULL, 'Hip-Hop', '2019', '../music_link/Hurts_Like_This.mp3'),
+(21, 'On & On (feat. Daniel Levi)', 'Cartoon', NULL, 'Pop', '2015', '../music_link/On_On.mp3'),
+(22, 'Heroes Tonight (feat. Johnning)', 'Janji', NULL, 'Pop', '2015', '../music_link/Heroes.mp3'),
+(23, 'Invincible', 'DEAF KEV', NULL, 'Electronic', '2015', '../music_link/Invincible.mp3'),
+(24, 'My Heart', 'Different Heaven & EH!DE', NULL, 'Pop', '2013', '../music_link/My_Heart.mp3'),
+(25, 'Blank', 'Disfigure', NULL, 'Electronic', '2013', '../music_link/Blank.mp3'),
+(26, 'Your Stories', 'Cartoon', 'The Story', 'EDM', '2021', '../music_link/Cartoon_YourStories.mp3'),
+(27, 'Hellcat', 'Desmeon', 'Demon Man', 'EDM', '2021', '../music_link/Desmeon_Hellcat.mp3'),
+(28, 'Undone', 'Desmeon', 'Demon Man', 'EDM', '2021', '../music_link/Desmeon_Undone.mp3'),
+(29, 'Little Bit', 'Futuristik', 'Big Data', 'EDM', '2013', '../music_link/Futuristik_LittleBit.mp3'),
+(30, 'A World Away', 'Inukshuk', 'Fantasy', 'EDM', '2015', '../music_link/Inukshuk_AWorldAway.mp3'),
+(31, 'Cant Wait', 'Jim Yosef', 'Anticipation', 'EDM', '2017', '../music_link/JimYosef_CantWait.mp3'),
+(32, 'Tell Me', 'Killercats', 'Kings', 'EDM', '2017', '../music_link/Killercats_TellMe.mp3'),
+(33, 'Lost', 'Kontinuum', 'Kaution', 'EDM', '2019', '../music_link/Kontinuum_Lost.mp3'),
+(34, 'Red Hands', 'Kontinuum', 'Kaution', 'EDM', '2019', '../music_link/Mendum_RedHands.mp3'),
+(35, 'Along The Road', 'NCT x T and Sugah', 'Journey', 'EDM', '2021', '../music_link/NCTxTandSugah_AlongTheRoad.mp3'),
+(36, 'MIKO', 'Phantom Sage', 'Ghost Story', 'EDM', '2019', '../music_link/PhantomSage_MIKO.mp3'),
+(37, 'Cut The Check', 'Spitfya x Desembra', 'Sad Times', 'EDM', '2020', '../music_link/SpitfyaXDesembra_CutTheCheck.mp3'),
+(38, 'Away', 'Spitfya x Desembra', 'Sad Times', 'EDM', '2020', '../music_link/Subtact_Away.mp3'),
+(39, 'To Myself', 'Uplink', 'Journals of Life', 'EDM', '2021', '../music_link/Uplink_ToMyself.mp3'),
+(40, 'Eternal Minds', 'Uplink', 'Journals of Life', 'EDM', '2021', '../music_link/Waysons_EternalMinds.mp3'),
+(41, 'Me You', 'Aden', NULL, 'Pop', '2022', '../music_link/Aden_Me_You.mp3'),
+(42, 'Happy', 'Aden', NULL, 'Pop', '2010', '../music_link/Aden_Happy.mp3'),
+(43, 'Night', 'Aden', NULL, 'Pop', '2010', '../music_link/Aden_Night.mp3'),
+(44, 'Back Home', 'Ghostrifter', NULL, 'Hip-Pop', '2021', '../music_link/Ghostrifter_Back_Home.mp3'),
+(45, 'Can You Feel The Love', 'Luke Bergs', NULL, 'Pop', '2018', '../music_link/Luke_Bergs_Can_You_Feel_The_Love.mp3'),
+(46, 'The way', 'LiQWYD', NULL, 'Hip-Hop', '2014', '../music_link/LiQWYD_The_way.mp3'),
+(47, 'Golden State of Mind', 'Luke Bergs', NULL, 'Pop', '2020', '../music_link/Luke_Bergs_Golden_State_of_Mind.mp3'),
+(48, 'I\'ll Be Your Sunshine', 'Luke Bergs', NULL, 'Pop', '2019', '../music_link/Luke_Bergs_Ill_Be_Your_Sunshine.mp3'),
+(49, 'Evans Soul Searching', 'Luke Bergs ft.Jenna', NULL, 'Pop', '2021', '../music_link/Luke_Bergs_Soul_Searching.mp3'),
+(50, 'Wanted', 'Purrple Cat', NULL, 'Hip-Pop', '2019', '../music_link/Purrple_Cat_Wanted.mp3');
 
 --
 -- Indexes for dumped tables
@@ -153,7 +112,7 @@ ALTER TABLE `music_information`
 -- AUTO_INCREMENT for table `music_information`
 --
 ALTER TABLE `music_information`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
